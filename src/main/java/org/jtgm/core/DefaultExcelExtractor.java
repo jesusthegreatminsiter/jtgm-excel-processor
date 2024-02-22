426affdad9b56b431cbd52bb2276029a427145e0
chiniebocalan <chinieann2024@gmail.com>
2024-02-22 21:33:51 +0800

    - creation of destination file

--- src/main/java/org/jtgm/core/DefaultExcelExtractor.java
+++ src/main/java/org/jtgm/core/DefaultExcelExtractor.java
@@ -26,30 +26,50 @@ public class DefaultExcelExtractor implements ExcelExtractor{
     @Override
     public void extract(MultipartFile file) {
         try {
-            Workbook workbook = StreamingReader.builder().open(file.getInputStream());
+            Workbook reqWorkbook = StreamingReader.builder().open(file.getInputStream());
 
-            Sheet sheet = workbook.getSheetAt(0);
+            Sheet sheet = reqWorkbook.getSheetAt(0);
 
             HashMap<String, Integer> headers = getHeaders(sheet);
+            List<FormExcelDTO> formExcelDTOList = getInfoFromExcel(sheet, headers);
 
-            List<FormExcelDTO> formExcelDTOList = new ArrayList<>();
-            for(Row row : sheet){
-                if(row.getRowNum() != HEADER_ROW_NUMBER){
-                    CellFinderDTO cellFinder = buildCellFinder(headers, row);
-                    formExcelDTOList.add(buildFormExcel(cellFinder, headerProperties));
-                }
-            }
-
-
-            File outputFile = new File(System.getProperty("user.dir") +"/final.xlsx");
-
-            FileOutputStream fos = new FileOutputStream(outputFile);
-            workbook.write(fos);
-
+            generateWorkBook(formExcelDTOList);
         }catch (Exception e) {
             e.printStackTrace();
             throw new GenericErrorException("Error occur", e);
         }
+    }
+
+    private void generateWorkBook(List<FormExcelDTO> formExcelList){
+        try {
+            File outputFile = new File(System.getProperty("user.dir") + "/final.xlsx");
+
+            FileOutputStream fos = new FileOutputStream(outputFile);
+            Workbook resWorkbook = null;
+            Sheet sheet = resWorkbook.getSheetAt(0);
+            for(int j = 0; j <= formExcelList.size(); j++){
+                Row row = sheet.createRow(sheet.getLastRowNum() + 1);
+                for(int i = 0; i <=3; i++){
+                    Cell cell = row.createCell(i);
+                    cell.setCellValue(formExcelList.get());
+                }
+            }
+            
+            resWorkbook.write(fos);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private List<FormExcelDTO>  getInfoFromExcel(Sheet sheet, HashMap<String, Integer> headers) {
+        List<FormExcelDTO> formExcelDTOList = new ArrayList<>();
+        for(Row row : sheet){
+            if(row.getRowNum() != HEADER_ROW_NUMBER){
+                CellFinderDTO cellFinder = buildCellFinder(headers, row);
+                formExcelDTOList.add(buildFormExcel(cellFinder, headerProperties));
+            }
+        }
+        return formExcelDTOList;
     }
 
     private HashMap<String, Integer> getHeaders(Sheet sheet){
