34b46531435fa8a26494b7757ef0c77bfe043918
chiniebocalan <chinieann2024@gmail.com>
2025-01-19 11:36:14 +0800

    bug fix for nsbp problem

--- src/main/java/org/jtgm/core/service/impl/DefaultExcelExtractor.java
+++ src/main/java/org/jtgm/core/service/impl/DefaultExcelExtractor.java
@@ -15,6 +15,9 @@ import java.io.InputStream;
 import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 
 @RequiredArgsConstructor
 @Slf4j
@@ -43,9 +46,9 @@ public class DefaultExcelExtractor implements ExcelExtractor {
             Files.createDirectories(Paths.get(System.getProperty("user.home") + "/JTGM MGroup/Processed"));
             File newFile = new File(System.getProperty("user.home") + "/JTGM MGroup/Processed/" + fileRaw.getName());
             File oldFile = new File(fileRaw.getPath());
-            File toDelete = oldFile;
-            oldFile.renameTo(newFile);
-            toDelete.delete();
+
+            log.info("[INFO] Moving files from Raw folder to processed.");
+            Files.move(oldFile.toPath(), newFile.toPath(), REPLACE_EXISTING);
         }catch (FileAlreadyExistsException ex){
             log.error("[ERROR] File already exist, will not transfer.");
         }catch (IOException ex){
